db81fa74472d5eac0f280f28d82f14092e529d3b
Densuke-fitness <densukefitness2020@gmail.com>
2021-04-27 21:17:30 +0900

    fix isssue#65: Created a transaction.

---
+++ model/gacha/gacha.go
@@ -0,0 +1,10 @@
+// //TODO:  not implemented
+package gacha
+
+// import (
+// 		"fmt"
+// 		"math/rand"
+
+// 		"github.com/Densuke-fitness/GoDojoTechTrain/model/auth"
+// 		logger "github.com/sirupsen/logrus"
+// )
