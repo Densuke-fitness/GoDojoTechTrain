49357897479ed0da4be9b88907b58b43e87f44fe
Densuke-fitness <yu.tamura@raksul.com>
2023-01-30 12:03:38 +0900

    refactor test method

--- testing.sh
+++ testing.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-echo "docker compose up -d go_db"
-docker-compose up -d go_db
+echo "docker compose -f docker-compose-local.yml up -d"
+docker compose -f docker-compose-local.yml up -d 
 
 echo "sleep 5s"
-sleep 5s
+sleep 5
 
-echo "go test ./... -v"
-go test ./... -v
+echo "docker exec test_go_api go test ./... -v"
+docker exec test_go_api go test ./... -v
 
 echo "docker compose down"
-docker-compose down
+docker-compose down --remove-orphan
 
 echo "docker volume rm godojotechtrain_db-volume"
 docker volume rm godojotechtrain_db-volume
